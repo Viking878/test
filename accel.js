e03c778f253376171bea1c0628c800630bb6e647
Erik <97635003+Viking878@users.noreply.github.com>
2023-10-12 12:55:35 +0200

    accel

---
+++ accel.js
@@ -0,0 +1,20 @@
+let y = 10; let r = 200; g = 180; b = 0;
+
+function draw() {
+    background(r, g, b);
+    strokeWeight(10);
+    ellipse(width / 2, y, 50);
+    if (ned)
+        y++;
+    else
+        y--;
+    if (y >= height || y <= 0)
+        ned = !ned;
+    if (accelerationX > 70) {
+        r = random(0, 256);
+        g = random(0, 256);
+        b = random(0, 256);
+        if(rystet%2 == 0)
+        ned = !ned;
+    }
+}
