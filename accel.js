a5c2190a707f2f3ec367dae6813498cf097f2684
Erik <erikhedebol@gmail.com>
2023-10-13 14:26:01 +0200

    md

--- accel.js
+++ accel.js
@@ -1,5 +1,29 @@
 let y = 10; let r = 200; g = 180; b = 0;
-//sjs
+let ned = true;
+let rystet = 0;
+let flyttet = 0;
+
+function setup() {
+    canvas = createCanvas(300, 550, 'beholder');
+    textSize(24);
+    // giver canvas border på 2 pixel, 
+    // og sørger derefter for at kanten tælles med i width
+    canvas.elt.style.border = '5px solid black';
+    canvas.elt.style.boxSizing = 'border-box';
+    canvas.elt.style.borderRadius = '20px';
+
+    canvas.parent('#beholder');
+    // gør canvas-elementet responsivt til skærmbredden
+    canvas.elt.style.width = '100%';
+    canvas.elt.style.height = '100%';
+
+    //bemærk at noden skal pakkes ud via .elt
+    const parentDiv = select('#beholder').elt;
+    const p = select('#test1').elt;
+    // indsæt canvas i ny position i rækkefølgen af elementer i div'en beholder
+    parentDiv.insertBefore(canvas.elt, p);
+}
+
 function draw() {
     background(r, g, b);
     strokeWeight(10);
@@ -16,5 +40,14 @@ function draw() {
         b = random(0, 256);
         if(rystet%2 == 0)
         ned = !ned;
+        rystet++;
     }
+    
+text('rystet: ' + str(rystet), 50, height-100);
+text('flyttet: ' + str(flyttet),50, height-50);
+}
+
+function deviceMoved(){
+    flyttet++;
+
 }
